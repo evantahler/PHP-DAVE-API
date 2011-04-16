3bbd144f4c1d19b80457ef98b97a9fb3b94dfa9a
Evan Tahler <e.tahler@modcloth.com>
2011-04-16 15:03:49 -0700

    fixing log path for SERVER

--- SERVER/SERVER.php
+++ SERVER/SERVER.php
@@ -18,7 +18,8 @@ How to test post: curl -d "param1=value1&param2=value2" http://localhost:3000/so
 ini_set("display_errors","1");
 error_reporting (E_ALL ^ E_NOTICE);
 $path = substr(__FILE__,0,(strlen(__FILE__) - strlen("SERVER.php")));
-chdir($path); unset($path); @mkdir("LOG");
+chdir($path); unset($path);
+
 require("server_config.php");
 date_default_timezone_set($SERVER['systemTimeZone']);
 
